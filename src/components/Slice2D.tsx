da3d2c803a125a26b981cd7c2d72dd2ed17df629
Stanislaw Swierc <stanislaw.swierc@gmail.com>
2026-03-03 21:08:32 -0500

    Show 4 digits on the axis

--- src/components/Slice2D.tsx
+++ src/components/Slice2D.tsx
@@ -99,7 +99,7 @@ export function Slice2D({ sim, rxIndex, t, onRxIndexChange }: Slice2DProps) {
         ]}
         layout={{
           xaxis: { title: t.axisF, type: 'log' },
-          yaxis: { title: t.axisCvc, tickformat: '.2e' },
+          yaxis: { title: t.axisCvc, tickformat: '.4e' },
           title: t.sliceCvcTitle(rxLabel),
           showlegend: false,
           autosize: true,
